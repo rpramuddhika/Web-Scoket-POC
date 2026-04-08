d770f00c2f6029660a792ba2b1512b4b72b71de6
rpramuddhika <rpramuddhika@miraisense.com>
2026-04-08 13:56:26 +0530

    wb added to the fe

--- client/src/renderer/src/App.tsx
+++ client/src/renderer/src/App.tsx
@@ -1,11 +1,44 @@
-import { useState } from 'react'
-import PageOne from './pages/PageOne'
-import PageTwo from './pages/PageTwo'
-import PageThree from './pages/PageThree'
+import { Client } from '@stomp/stompjs'
+import { useEffect, useState } from 'react'
+import { toast } from 'react-toastify'
 import Notification from './pages/Notification'
+import PageOne from './pages/PageOne'
+import PageThree from './pages/PageThree'
+import PageTwo from './pages/PageTwo'
+
+type NotificationMessage = {
+  id: number
+  isRemember: boolean
+  notificationTime: string
+}
 
 function App(): React.JSX.Element {
   const [activePage, setActivePage] = useState<'one' | 'two' | 'three' | 'notification'>('one')
+
+  useEffect(() => {
+    const client = new Client({
+      brokerURL: 'ws://localhost:8080/ws/websocket',
+      reconnectDelay: 5000,
+      onConnect: () => {
+        client.subscribe('/topic/notifications', (message) => {
+          const body: NotificationMessage = JSON.parse(message.body)
+          toast.info(`Notification at ${body.notificationTime}`, {
+            position: 'top-right',
+            autoClose: 5000
+          })
+        })
+      },
+      onStompError: (frame) => {
+        console.error('STOMP error', frame.headers['message'])
+      }
+    })
+
+    client.activate()
+
+    return () => {
+      client.deactivate()
+    }
+  }, [])
 
   const renderPage = (): React.JSX.Element => {
     switch (activePage) {
